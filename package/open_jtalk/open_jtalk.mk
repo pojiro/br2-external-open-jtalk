ba37eea93e81a7ccbd07be9531e9f2eab69ccb4c
Ryota Kinukawa <kinukawa@tombo-works.com>
2025-08-31 16:07:50 +0900

    Fix to build open-jtalk

--- package/open_jtalk/open_jtalk.mk
+++ package/open_jtalk/open_jtalk.mk
@@ -5,11 +5,12 @@
 ################################################################################
 OPEN_JTALK_VERSION = 1.11
 OPEN_JTALK_SOURCE = open_jtalk-$(OPEN_JTALK_VERSION).tar.gz
-OPEN_JTALK_SITE = https://sourceforge.net/projects/open-jtalk/files/Open%20JTalk/open_jtalk-$(OPEN_JTALK_VERSION)/
+OPEN_JTALK_SITE = https://sourceforge.net/projects/open-jtalk/files/Open%20JTalk/open_jtalk-$(OPEN_JTALK_VERSION)
 OPEN_JTALK_LICENSE = BSD-3-Clause
 OPEN_JTALK_LICENSE_FILES = COPYING
+OPEN_JTALK_DEPENDENCIES = hts_engine_API
 OPEN_JTALK_INSTALL_STAGING = NO
 OPEN_JTALK_INSTALL_TARGET = YES
-OPEN_JTALK_CONF_OPTS = --with-hts-engine-header-path=/usr/local/hts_engine_API/include --with-hts-engine-library-path=/usr/local/hts_engine_API/lib
+OPEN_JTALK_CONF_OPTS = --with-hts-engine-header-path=$(STAGING_DIR)/usr/include --with-hts-engine-library-path=$(STAGING_DIR)/usr/lib
 
 $(eval $(autotools-package))
